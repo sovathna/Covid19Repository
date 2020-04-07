208eecdd32062dff1e8c6da29578fad4f5f45bea
Sovathna Hong <hongsovathna@gmail.com>
2020-04-07 16:59:42 +0700

    updates

--- lib/model/list_total_data_event.dart
+++ lib/model/list_total_data_event.dart
@@ -9,3 +9,19 @@ abstract class ListTotalDataEvent extends Event {
 class GetListTotalDataEvent extends ListTotalDataEvent {
   const GetListTotalDataEvent();
 }
+
+@immutable
+class FilterListTotalDataEvent extends ListTotalDataEvent {
+  final String filter;
+  const FilterListTotalDataEvent(this.filter);
+  @override
+  List<Object> get props => [filter];
+}
+
+@immutable
+class ChangeListTotalDataEvent extends ListTotalDataEvent {
+  final bool isFilter;
+  const ChangeListTotalDataEvent(this.isFilter);
+  @override
+  List<Object> get props => [isFilter];
+}
